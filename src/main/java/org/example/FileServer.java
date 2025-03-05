7251f5aa6ff32a3813bcea2cea9fe7ce77e29157
Daan Dekoning Krekels <daan.dekoningkrekels@student.ap.be>
2025-03-05 20:25:39 +0100

    Add threading to server

--- src/main/java/org/example/FileServer.java
+++ src/main/java/org/example/FileServer.java
@@ -7,41 +7,67 @@ import java.net.Socket;
 public class FileServer {
     private ServerSocket serverSocket;
 
-        public void start(int port) throws IOException {
-                this.serverSocket = new ServerSocket(port);
-                this.clientSocket = this.serverSocket.accept();
-                this.out = this.clientSocket.getOutputStream();
-                this.in = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+    public void start(int port) throws IOException {
+        this.serverSocket = new ServerSocket(port);
+        System.out.println("Server started on port " + port);
 
-                String filename = in.readLine();
-                System.out.println("Requested file: "+filename);
-                File myFile = new File("server/"+filename);
-                System.out.println(myFile.getAbsolutePath());
-                if (myFile.length() == 0){
-                    System.out.println("File doesn't exist!");
-                } else{
-                    FileInputStream  fis = new FileInputStream(myFile);
-                    BufferedOutputStream bos = new BufferedOutputStream(this.clientSocket.getOutputStream());
+        while (true) {
+            Socket clientSocket = this.serverSocket.accept();
+            System.out.println("New client connected");
+            new ClientHandler(clientSocket).start();
+        }
+    }
 
-                    byte[] buffer = new byte[4096];
-                    int bytesRead;
-                    while ((bytesRead = fis.read(buffer)) != -1) {
-                        bos.write(buffer, 0, bytesRead);
-                    }
-                    fis.close();
-                    bos.flush();
-                    bos.close();
-                    System.out.println("File sent successfully!");
+    public void stop() throws IOException {
+        this.serverSocket.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        FileServer server = new FileServer();
+        server.start(6666);
+    }
+}
+
+class ClientHandler extends Thread {
+    private Socket clientSocket;
+    private OutputStream out;
+    private BufferedReader in;
+
+    public ClientHandler(Socket socket) {
+        this.clientSocket = socket;
+    }
+
+    public void run() {
+        try {
+            this.out = this.clientSocket.getOutputStream();
+            this.in = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+
+            String filename = in.readLine();
+            System.out.println("Requested file: " + filename);
+            File myFile = new File("server/" + filename);
+            System.out.println(myFile.getAbsolutePath());
+            if (!myFile.exists() || myFile.length() == 0) {
+                System.out.println("File doesn't exist!");
+            } else {
+                FileInputStream fis = new FileInputStream(myFile);
+                BufferedOutputStream bos = new BufferedOutputStream(this.clientSocket.getOutputStream());
+
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                while ((bytesRead = fis.read(buffer)) != -1) {
+                    bos.write(buffer, 0, bytesRead);
                 }
-
-                this.stop();
+                fis.close();
+                bos.flush();
+                bos.close();
+                System.out.println("File sent successfully!");
             }
 
-        public void stop() throws IOException {
             this.in.close();
             this.out.close();
             this.clientSocket.close();
-            this.serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
