0183c3d6159bc64e727ccb8e76f3d2585480da31
WarreH <warrehofmans@gmail.com>
2025-03-05 20:13:01 +0100

    remove unused imports

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,9 @@
 package org.example;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
-//        FileClient client = new FileClient();
-//        client.startConnection("127.0.0.1", 6666);
-//        String filename = "test.html";
-//        String response = client.sendMessage(filename);
-//        System.out.println(response);
 
         for(int i =0;i<10;i++){
             FileClient client = new FileClient();
