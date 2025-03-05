6d2dbc9e5293b201c1630b33d870ed10f02a2547
WarreH <warrehofmans@gmail.com>
2025-03-05 20:28:25 +0100

    added different file sizes request

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,11 +8,10 @@ public class Main {
         for(int i =0;i<10;i++){
             FileClient client = new FileClient();
             client.startConnection("127.0.0.1", 6666);
-            String filename = "payload.png";
-            String localName = "payload"+i+".png";
-            client.requestFile(filename,localName);
+            String filename = "payload"+i%4+".txt";
+            client.requestFile(filename,filename);
             client.stopConnection();
-            System.out.println("Processed client "+i);
+            System.out.println("Processed client "+i+" with payload "+i%4);
         }
 
     }
